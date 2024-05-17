19cab4f6accd833b3adb25af250970dad3d0935c
LuilliAWSimp <luilliielsexy@gmail.com>
2024-05-17 13:47:18 -0600

    a
    a

---
+++ tatewari/app/(tabs)/other.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export default class Other extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Otra pantalla así rocotota</Text>
+      </View>
+    );
+  }
+}
